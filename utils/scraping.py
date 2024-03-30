4dffb2922cc6019ba5e8672ff780e424b5e29ac7
LucasChasseguet <lucas@data-and-data.com>
2024-03-30 23:49:55 +0100

    Made some refactoring / added a new class to store the urls and xpaths

--- utils/scraping.py
+++ utils/scraping.py
@@ -11,97 +11,102 @@ from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
 # Api , scraping
 from bs4 import BeautifulSoup
-import lxml
+from lxml import html
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By 
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.common.exceptions import TimeoutException , NoAlertPresentException
 import yfinance as yf
 
-import time
+
 yf.pdr_override()
 
 # Get rid of the scientific notation
 pd.set_option('display.float_format', lambda x: '%.2f' % x)
 
+from utils.datacontainer import DataContainer
+
 class ScrapingSelenium:
+
+    datacontainer = DataContainer()
 
     def __init__(self , company_name , ticker):
         self.company_name = company_name
         self.ticker = ticker
-        self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+        self.driver = webdriver.Chrome() # Old version of selenium ; service=Service(ChromeDriverManager().install())
 
 
     def get_url(self) -> tuple:
-            """ 
-            Get the zone bourse urls (fondamentals and society) using selenium
-            """
+        """ 
+        Get the zone bourse urls (fondamentals and society) using selenium
+        """
+        updated_zone_bourse_url = ScrapingSelenium.datacontainer.BASE_URL_ZONE_BOURSE.format(self.company_name)
+        self.driver.get(updated_zone_bourse_url)
 
-            self.driver.get(f'https://www.google.com/search?q=zone+bourse+{self.company_name}+finance&sxsrf=ALiCzsbIaWNWrnXJ5acLqlPx2kINT72YMA%3A1670610120483&ei=yHyTY9CSHcmPkdUP3veM2AI&oq=zone+bourse+telenor++finance&gs_lcp=Cgxnd3Mtd2l6LXNlcnAQAxgAMgQIIxAnOggIABCiBBCwA0oECEEYAUoECEYYAFCMBViMBWCHEGgBcAB4AIABKYgBKZIBATGYAQCgAQHIAQPAAQE&sclient=gws-wiz-serp')
+        try: # Do the capcha if needed
+            WebDriverWait(self.driver, 15).until(
+            EC.presence_of_element_located((By.XPATH, "(//button)[4]"))
+        ).click()
 
-            try: # Do the capcha if needed
-                WebDriverWait(self.driver, 15).until(
-                EC.presence_of_element_located((By.XPATH, "(//button)[4]"))
-            ).click()
+        except TimeoutException:
+            pass 
 
-            except TimeoutException:
-                pass 
+        # Query the first link
+        link_first_result = self.driver.find_element(By.XPATH , ('(//div[contains(@data-async-context , "query:zone")]/div//a)[1]')).get_attribute("href")
+        self.driver.get(link_first_result)
 
-            # Query the first link
-            link_first_result = self.driver.find_element(By.XPATH , ('(//div[contains(@data-async-context , "query:zone")]/div//a)[1]')).get_attribute("href")
-            self.driver.get(link_first_result)
+        time.sleep(2)
 
-            time.sleep(2)
+        try:
+            # Desactivate the alert
+            alert = self.driver.switch_to.alert
+            alert.accept()
+        except NoAlertPresentException:
+            pass
 
-            try:
-                # Desactivate the alert
-                alert = self.driver.switch_to.alert
-                alert.accept()
-            except NoAlertPresentException:
-                pass
+        current_url = self.driver.current_url
 
-            current_url = self.driver.current_url
+        splitted_url = current_url.split('/')
 
-            splitted_url = current_url.split('/')
 
-            if splitted_url[-2] not in ('fondamentaux' , 'societe' , "cotations"):
+        match splitted_url[-2]:
+            
+            case w if w not in ('fondamentaux' , 'societe' , "cotations"):
                 url_desc = current_url + 'societe/'
                 url = current_url + 'fondamentaux/'
 
-            elif splitted_url[-2] == "cotations":
+            case "cotations":
                 url = current_url.replace("cotations/" , 'fondamentaux/')
                 url_desc = current_url.replace("cotations/" , 'societe/')
 
-            elif splitted_url[-2] == 'fondamentaux':
+            case 'fondamentaux':
                 url_desc = current_url.replace(splitted_url[-2] , 'societe')
                 url = current_url
 
-            elif splitted_url[-2] == 'societe':
+            case 'societe':
                 url = current_url.replace("societe/" , 'fondamentaux/')
                 url_desc = current_url
 
-            self.title = str(url.split('/')[-3]) 
-            self.url = url
-            self.url_desc = url_desc
+        self.title = str(url.split('/')[-3]) 
+        self.url = url
+        self.url_desc = url_desc
 
-            return str(url.split('/')[-3]) , url , url_desc 
+        return str(url.split('/')[-3]) , url , url_desc 
 
 
 
 
     def get_tables(self) -> tuple[pd.DataFrame , pd.DataFrame , pd.DataFrame , pd.DataFrame]:
         
-        def clean_and_set_index(df : pd.DataFrame) -> pd.DataFrame:
+        def __clean_and_set_index(df : pd.DataFrame) -> pd.DataFrame:
             index_col = df.columns[df.columns.str.startswith('Période')]
             df.index = df[index_col]
             df = df.drop(index_col , axis=1)
             df.index = [re.sub(string=re.sub(string=str(ind).strip() , repl='' , pattern="(\"|\(|\)|,|\\d)") , repl='' , pattern=r"^(?:')|(?:')$").strip() for ind in df.index]
             return df.drop(df.index[-1])
         
-        def clean_table_apply(x):
+        def __clean_table(x):
             return str(x).replace('\u202f' , "").replace('%' , '').replace(',' , '.').replace('x' , '').replace(' ' , '')
 
         # Get the source code 
@@ -118,10 +123,10 @@ class ScrapingSelenium:
         table_2 = pd.read_html(str(tables[2]).replace("," , "."))[1].apply(lambda x : x.replace('-' , None))
         table_3 = pd.read_html(str(tables[3]).replace("," , "."))[1].apply(lambda x : x.replace('-' , None))
 
-        table_0 = clean_and_set_index(table_0).fillna(np.nan).map(lambda x : float(clean_table_apply(x)) if clean_table_apply(x) else np.nan)
-        table_1 = clean_and_set_index(table_1).fillna(np.nan).map(lambda x : float(clean_table_apply(x)) if clean_table_apply(x) else np.nan)
-        table_2 = clean_and_set_index(table_2).fillna(np.nan).map(lambda x : float(clean_table_apply(x)) if clean_table_apply(x) else np.nan)
-        table_3 = clean_and_set_index(table_3).fillna(np.nan).map(lambda x : float(clean_table_apply(x)) if clean_table_apply(x) else np.nan)
+        table_0 = __clean_and_set_index(table_0).fillna(np.nan).map(lambda x : float(__clean_table(x)) if __clean_table(x) else np.nan)
+        table_1 = __clean_and_set_index(table_1).fillna(np.nan).map(lambda x : float(__clean_table(x)) if __clean_table(x) else np.nan)
+        table_2 = __clean_and_set_index(table_2).fillna(np.nan).map(lambda x : float(__clean_table(x)) if __clean_table(x) else np.nan)
+        table_3 = __clean_and_set_index(table_3).fillna(np.nan).map(lambda x : float(__clean_table(x)) if __clean_table(x) else np.nan)
 
         # Special treatment on table_3 to compute the Capitaux propres , that have been removed from zone bourse , so we compute it here
         values_ = (table_3.loc["Total Actifs"].astype(float) - table_3.loc["Dette Nette"].astype(float)).values
@@ -133,10 +138,10 @@ class ScrapingSelenium:
         return table_0 , table_1 , table_2 , table_3
 
     def get_description(self , url_desc) -> str:
-        
+
         self.driver.get(url_desc)
-        tree = lxml.html.fromstring(self.driver.page_source)
-        xpath_expression = '//div[@class="company-logo"]/following-sibling::text()'
+        tree = html.fromstring(self.driver.page_source)
+        xpath_expression = ScrapingSelenium.datacontainer.GET_DESCRIPTION_XPATH # '//div[@class="company-logo"]/following-sibling::text()'
         raw_text = tree.xpath(xpath_expression)
         pattern = re.compile(r'\\n|(<.*?>)|\[|\]|\"')
         return re.sub(pattern=pattern , string=str(raw_text) , repl='').strip()
@@ -144,17 +149,16 @@ class ScrapingSelenium:
 
 
     def sentiment_scores(self) -> pd.DataFrame:
-        '''Sentiment analysis using vader SentimentAnalyser (code source modified to fit my needs) , using finviz news title concerning the companie.'''
-        if '.' in self.ticker:
-            ticker_ = self.ticker.split('.')[0]
-            url = f'https://finviz.com/quote.ashx?t={ticker_}&p=d'
-        else:
-            url = f'https://finviz.com/quote.ashx?t={self.ticker}&p=d'
-
+        """
+        Sentiment analysis using vader SentimentAnalyser using finviz news titles concerning the company
+        """
         
+        ticker = self.ticker.split('.')[0] if '.' in self.ticker else self.ticker 
+        url = ScrapingSelenium.BASE_URL_FINVIZ.format(ticker)
+
         self.driver.get(url)
         time.sleep(5)
-        self.driver.find_element(By.XPATH , ('//*[@id="qc-cmp2-ui"]/div[2]/div/button[3]')).click()
+        self.driver.find_element(By.XPATH , (ScrapingSelenium.datacontainer.FINVIZ_POPUP_XPATH)).click() # '//*[@id="qc-cmp2-ui"]/div[2]/div/button[3]'
         time.sleep(3)
         source = self.driver.page_source
         self.driver.quit()
