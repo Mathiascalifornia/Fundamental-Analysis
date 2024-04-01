aaae4a2f2e075f214d3b2cd56b6ea3e6b4f6946a
LucasChasseguet <lucas@data-and-data.com>
2024-04-01 19:59:21 +0200

    Fixed the compound interest error (used Adj Close instead of Close , which gave crazy results)

--- app_fund_analysis/compute_dividend_gain_over_n_period.py
+++ app_fund_analysis/compute_dividend_gain_over_n_period.py
@@ -20,7 +20,7 @@ class DividendGainCalculator:
     """
     
     # Old companies , highly representative of what we except from a good dividend company
-    BENCHMARK_TICKERS = ("KO" , "JNJ" , "XOM" , "MMM" , "ITW" , "IBM" , "ED" , "O" , "PG" , "EPD" , "BLK" , "VZ" , "NWN")
+    BENCHMARK_TICKERS = ("KO" , "JNJ" , "XOM" , "MMM" ,  "ITW" , "IBM" , "O" , "PG" , "EPD" , "BLK" , "VZ" , "NWN" , "HD" , "LEG")
     MINUS_YEARS_TO_COMPUTE = (3 , 5 , 10 , 15 , 20)
     BENCHMARK_FOLDER = "..\\data\\benchmark_backtesting_scores"
 
@@ -66,7 +66,7 @@ class DividendGainCalculator:
                     results_by_year[year].append(gain_year)
 
             
-            results_by_year = {year : round(np.mean(results_by_year[year])) for year in results_by_year.keys()}
+            results_by_year = {year : round(np.median(results_by_year[year])) for year in results_by_year.keys()}
             df_results = pd.DataFrame(results_by_year.items() , columns=["Years of investment", "Gains benchmark"])
 
             DividendGainCalculator.pickle_loader.save_pickle_object(obj=df_results , file_path=path_pickle_results_current_year)
@@ -83,53 +83,50 @@ class DividendGainCalculator:
         merged_df = self.merge_price_and_div_df(df_price=self.df_price , 
                                                 df_div=self.df_div)
         
-        
         # Set the dataframe in the correct time span
         merged_df = self.cut_current_year_values(merged_df=merged_df)
 
-        computed_df = self.compute_compound_interest(merged_df=merged_df)
-
-        return self.get_yearly_gains(computed_df=computed_df)
+        return self.get_yearly_gains(merged_df=merged_df)
 
 
     @staticmethod
-    def get_yearly_gains(computed_df:pd.DataFrame) -> pd.DataFrame:
+    def get_yearly_gains(merged_df:pd.DataFrame) -> pd.DataFrame:
 
-        def __check_enought_time_horizon(computed_df:pd.DataFrame , minus_n_years:int) -> bool:
-            return round((computed_df.index[-1] - computed_df.index[0]).days / 365.25) >= minus_n_years
+        def __check_enought_time_horizon(merged_df:pd.DataFrame , minus_n_years:int) -> bool:
+            return round((merged_df.index[-1] - merged_df.index[0]).days / 365.25) >= minus_n_years
         
+        def __subset_over_minus_n_years(minus_n_years:Optional[int] , 
+                                        merged_df:pd.DataFrame) -> pd.DataFrame:
+            
+            """ 
+            Return the merged dataframe minus n years
+            """
+            if minus_n_years is None:
+                return merged_df
+            
+            date_to_subset = merged_df.index[-1]
+            lower_boundry = date_to_subset - datetime.timedelta(days=minus_n_years*365)
+            return merged_df[str(lower_boundry):]
+
+
         results = {}
         for year in DividendGainCalculator.MINUS_YEARS_TO_COMPUTE:
 
-            if __check_enought_time_horizon(computed_df=computed_df , minus_n_years=year):
-                minus_n_df = DividendGainCalculator.subset_over_minus_n_years(minus_n_years=year , 
-                                                                            computed_df=computed_df)
+            if __check_enought_time_horizon(merged_df=merged_df , minus_n_years=year):
+
+                right_range_merged_df = __subset_over_minus_n_years(minus_n_years=year , 
+                                                                    merged_df=merged_df)
                 
                 
-                total_gains = minus_n_df.iloc[-1]["Capital"] - minus_n_df.iloc[0]["Capital"]
+                computed_df = DividendGainCalculator.compute_compound_interest(merged_df=right_range_merged_df)
+
+                total_gains = computed_df.iloc[-1]["Capital"] - computed_df.iloc[0]["Capital"]
                 results[year] = total_gains
             else:
                 results[year] = np.nan
 
         return pd.DataFrame(results.items(), columns=["Years of investment", "Gains"])
 
-
-        
-
-    
-    @staticmethod
-    def subset_over_minus_n_years(minus_n_years:Optional[int] , 
-                                  computed_df:pd.DataFrame) -> pd.DataFrame:
-        
-        """ 
-        Return the merged dataframe minus n years
-        """
-        if minus_n_years is None:
-            return computed_df
-        
-        date_to_subset = computed_df.index[-1]
-        lower_boundry = date_to_subset - datetime.timedelta(days=minus_n_years*365)
-        return computed_df[str(lower_boundry):]
 
     @staticmethod
     def cut_current_year_values(merged_df:pd.DataFrame) -> pd.DataFrame:
@@ -166,7 +163,7 @@ class DividendGainCalculator:
         merged_df["N shares"] = 0
 
         merged_df.iloc[0 , merged_df.columns.get_loc("Capital")] = initial_capital # Set the initial capital
-        merged_df.iloc[0 , merged_df.columns.get_loc("N shares")] = merged_df.iloc[0]["Capital"] / merged_df.iloc[0]["Adj Close"] # Initial number of shares
+        merged_df.iloc[0 , merged_df.columns.get_loc("N shares")] = merged_df.iloc[0]["Capital"] / merged_df.iloc[0]["Close"] # Initial number of shares
 
         for i in range(1 , len(merged_df)):
 
@@ -175,7 +172,7 @@ class DividendGainCalculator:
 
             gain = previous["N shares"] * current["Dividends"]
             new_capital = previous["Capital"] + gain 
-            new_n_shares = previous["N shares"] + (gain / current["Adj Close"]) 
+            new_n_shares = previous["N shares"] + (gain / current["Close"]) 
 
 
             merged_df.iloc[i , merged_df.columns.get_loc("Capital")] = new_capital
--- app_fund_analysis/dividend_score_calculator.py
+++ app_fund_analysis/dividend_score_calculator.py
@@ -1,6 +1,5 @@
-from typing import Dict , Union , Tuple , Any
+from typing import Dict , Union , Tuple
 import datetime as dt
-import pickle
 import os 
 
 import pandas as pd 
@@ -8,6 +7,7 @@ import numpy as np
 
 from api_calls import ApiCaller
 from pickle_loader import PickleLoaderAndSaviour
+
 
 class DividendScoreCalculator:
 
@@ -18,7 +18,7 @@ class DividendScoreCalculator:
     STABILITY_SCORE_WEIGHT = 1
     STRIKE_WEIGHT = 0.5
 
-    BENCHMARK_FOLDER = "..\\data\\benchmark_dividends_scores"
+    BENCHMARK_FOLDER = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'data')) # "..\\data\\benchmark_dividends_scores"
 
     pickle_loader = PickleLoaderAndSaviour()
 
